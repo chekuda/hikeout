14be6a60ca2d0af3907b276a6b9fdbcaaa03ec8c
rameshanand <3526682+rameshanandakrishnan@users.noreply.github.com>
2018-09-13 17:31:47 +0100

    update

--- src/client/pages/_app.js
+++ src/client/pages/_app.js
@@ -1,65 +1,51 @@
 import React from 'react';
-import Head from 'next/head';
-import App, { Container, createUrl } from 'next/app';
-
+import App, { Container } from 'next/app';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { createMuiTheme } from '@material-ui/core/styles';
+import JssProvider from 'react-jss/lib/JssProvider';
+import getPageContext from '../getPageContext';
 
-const theme = () => {
-  return createMuiTheme({
-    palette: {
-      primary: {
-        main: '#0097a7'
-      },
-      secondary: {
-        main: '#ffca28'
-      },
-      text: {
-        primary: 'rgba(0, 0, 0, 0.75)'
-      },
-      warning: {
-        main: '#ffa000'
-      },
-      success: {
-        main: '#43a047'
-      },
-      info: {
-        main: '#1976d2'
-      }
-    }
-  });
-};
+class MyApp extends App {
+  constructor(props) {
+    super(props);
+    this.pageContext = getPageContext();
+  }
 
-export default class MyApp extends App {
-  state = {
-    sheetsManager: this.props.sheetsManager || new Map()
-  };
+  pageContext = null;
 
   componentDidMount() {
+    // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side');
-
     if (jssStyles && jssStyles.parentNode) {
       jssStyles.parentNode.removeChild(jssStyles);
     }
   }
 
   render() {
-    const { Component, router } = this.props;
-    const { sheetsManager } = this.state;
-
+    const { Component, pageProps } = this.props;
     return (
       <Container>
-        <MuiThemeProvider theme={theme()} sheetsManager={sheetsManager}>
-          <Head>
-            <title>HIKEOUT</title>
-            <meta charSet="utf-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-          </Head>
-          <CssBaseline />
-          <Component />
-        </MuiThemeProvider>
+        {/* Wrap every page in Jss and Theme providers */}
+        <JssProvider
+          registry={this.pageContext.sheetsRegistry}
+          generateClassName={this.pageContext.generateClassName}
+        >
+          {/* MuiThemeProvider makes the theme available down the React
+              tree thanks to React context. */}
+          <MuiThemeProvider
+            theme={this.pageContext.theme}
+            sheetsManager={this.pageContext.sheetsManager}
+          >
+            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            <CssBaseline />
+            {/* Pass pageContext to the _document though the renderPage enhancer
+                to render collected styles on server side. */}
+            <Component pageContext={this.pageContext} {...pageProps} />
+          </MuiThemeProvider>
+        </JssProvider>
       </Container>
     );
   }
 }
+
+export default MyApp;
--- src/client/pages/index.js
+++ src/client/pages/index.js
@@ -1,27 +1,81 @@
-import React, { Component, Fragment } from 'react';
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
 
-import Header from '../components/Header';
-
-import './index.css';
-
-class App extends Component {
-  static async getInitialProps() {
-    return {
-      name: 'Next React App'
-    };
+const styles = theme => ({
+  root: {
+    textAlign: 'center',
+    paddingTop: theme.spacing.unit * 20
+  },
+  card: {
+    maxWidth: 345
+  },
+  media: {
+    height: 140
   }
+});
+
+class Index extends React.Component {
+  state = {
+    open: false
+  };
+
+  handleClose = () => {
+    this.setState({
+      open: false
+    });
+  };
+
+  handleClick = () => {
+    this.setState({
+      open: true
+    });
+  };
 
   render() {
+    const { classes } = this.props;
+    const { open } = this.state;
+
     return (
-      <Fragment>
-        <Header />
-        <div className="index__container">
-          <div>Hello {this.props.name}.</div>
-          <div>I am the "index.js" page. Modify me and see what happens.</div>
-        </div>
-      </Fragment>
+      <div className={classes.root}>
+        <Typography variant="display1" gutterBottom>
+          Material-UI
+        </Typography>
+        <Card className={classes.card}>
+          <CardActionArea>
+            <CardMedia
+              className={classes.media}
+              image="/static/images/cards/contemplative-reptile.jpg"
+              title="Contemplative Reptile"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="headline" component="h2">
+                Lizard
+              </Typography>
+              <Typography component="p">
+                Lizards are a widespread group of squamate reptiles, with over 6,000 species,
+                ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              Share
+            </Button>
+            <Button size="small" color="primary">
+              Learn More
+            </Button>
+          </CardActions>
+        </Card>
+      </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(Index);
