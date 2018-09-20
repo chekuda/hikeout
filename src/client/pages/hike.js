1a1b66e18b50174c062d197a01cfe6d3fac294c1
rameshanand <3526682+rameshanandakrishnan@users.noreply.github.com>
2018-09-20 18:07:35 +0100

    update

--- src/client/pages/hike.js
+++ src/client/pages/hike.js
@@ -1,7 +1,6 @@
-import React, { Component, Fragment } from 'react'
-import { withStyles } from '@material-ui/core/styles'
-import Typography from '@material-ui/core/Typography'
-
+import React, { Component, Fragment } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 
 const styles = theme => ({
   topSection: {
@@ -13,38 +12,36 @@ const styles = theme => ({
     color: theme.palette.primary.dark,
     textTransform: 'uppercase',
     letterSpacing: '2.5px'
-  },
-})
+  }
+});
 
 class Hike extends Component {
-  state = {}
+  state = {};
 
-  componentDidMount(){
-    const currentHikeId = new URL(window.location).searchParams.get('id')
+  componentDidMount() {
+    const currentHikeId = new URL(window.location).searchParams.get('id');
 
-    fetch('https://granite-camilla.codio.io/hikeData.json#')
+    fetch(`/api/hikes/${currentHikeId}`)
       .then(res => res.json())
       .then(data => {
         this.setState({
           currentHike: data.europe.find(ele => ele.id == currentHikeId)
-        })
-      })
+        });
+      });
   }
 
   render() {
-    const { classes } = this.props
-    if(!this.state.currentHike) return null
+    const { classes } = this.props;
+    if (!this.state.currentHike) return null;
 
     return (
       <Fragment>
         <Typography className={classes.topSection} variant="title" component="h1">
-            {
-              this.state.currentHike.title
-            }
+          {this.state.currentHike.title}
         </Typography>
       </Fragment>
-    )
+    );
   }
 }
 
-export default withStyles(styles)(Hike)
+export default withStyles(styles)(Hike);
