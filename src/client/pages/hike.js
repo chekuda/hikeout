3a6079b1885483ba4f5bfe972691abdf9a897528
Jose Luis Checa Exposito <joselchecaexpo@gmail.com>
2018-09-20 18:04:44 +0100

    add hikes id

--- src/client/pages/hike.js
+++ src/client/pages/hike.js
@@ -1,9 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+
+
+const styles = theme => ({
+  topSection: {
+    width: '100%',
+    padding: '30px',
+    textAlign: 'center',
+    fontSize: '30px',
+    fontWeight: 'bold',
+    color: theme.palette.primary.dark,
+    textTransform: 'uppercase',
+    letterSpacing: '2.5px'
+  },
+})
 
 class Hike extends Component {
+  state = {}
+
+  componentDidMount(){
+    const currentHikeId = new URL(window.location).searchParams.get('id')
+
+    fetch('https://granite-camilla.codio.io/hikeData.json#')
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          currentHike: data.europe.find(ele => ele.id == currentHikeId)
+        })
+      })
+  }
+
   render() {
-    return <div>Hike</div>;
+    const { classes } = this.props
+    if(!this.state.currentHike) return null
+
+    return (
+      <Fragment>
+        <Typography className={classes.topSection} variant="title" component="h1">
+            {
+              this.state.currentHike.title
+            }
+        </Typography>
+      </Fragment>
+    )
   }
 }
 
-export default Hike;
+export default withStyles(styles)(Hike)
